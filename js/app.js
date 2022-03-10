b070ea2251aad15c42c81fa8fb37206f51c6b489
rakibul-wdp <rakibul.wdp@gmail.com>
2022-03-11 03:17:45 +0600

    add liked and report page heading

--- js/app.js
+++ js/app.js
@@ -35,16 +35,22 @@ const switchTab = (id) => {
     document.getElementById('posts').style.display = 'grid';
     document.getElementById('liked').style.display = 'none';
     document.getElementById('reported').style.display = 'none';
+    document.getElementById('liked-heading').style.display = 'none';
+    document.getElementById('report-heading').style.display = 'none';
   } else if (id === 'liked') {
     document.getElementById('liked').style.display = 'block';
     document.getElementById('posts').style.display = 'none';
     document.getElementById('reported').style.display = 'none';
+    document.getElementById('liked-heading').style.display = 'block';
+    document.getElementById('report-heading').style.display = 'none';
 
     displayLikedPosts();
   } else {
     document.getElementById('reported').style.display = 'block';
     document.getElementById('posts').style.display = 'none';
     document.getElementById('liked').style.display = 'none';
+    document.getElementById('liked-heading').style.display = 'none';
+    document.getElementById('report-heading').style.display = 'block';
 
     displayReportedPosts();
   }
